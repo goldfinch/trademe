02d81501237e5dc3ee4e3ac30408c156a044198b
Art Karasev <goldfinch@users.noreply.github.com>
2024-02-10 10:39:53 +1300

    WIP

--- src/Blocks/TrademeBlock.php
+++ src/Blocks/TrademeBlock.php
@@ -11,26 +11,11 @@ class TrademeBlock extends BaseElement
     private static $singular_name = 'Trademe';
     private static $plural_name = 'Trademes';
 
-    private static $db = [];
-
     private static $inline_editable = false;
-    private static $description = '';
     private static $icon = 'font-icon-p-shop';
 
     public function Items($limit)
     {
         return TrademeItem::get()->limit($limit);
-    }
-
-    public function getSummary()
-    {
-        return $this->getDescription();
-    }
-
-    public function getType()
-    {
-        $default = $this->i18n_singular_name() ?: 'Block';
-
-        return _t(__CLASS__ . '.BlockType', $default);
     }
 }
