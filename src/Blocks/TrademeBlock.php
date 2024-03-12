15ebd3302fe838e560b13342da12f088fdef0fae
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-03-12 14:36:37 +1300

    WIP

--- src/Blocks/TrademeBlock.php
+++ src/Blocks/TrademeBlock.php
@@ -4,14 +4,18 @@ namespace Goldfinch\Trademe\Blocks;
 
 use Goldfinch\Trademe\Models\TrademeItem;
 use DNADesign\Elemental\Models\BaseElement;
+use Goldfinch\Helpers\Traits\BaseElementTrait;
 
 class TrademeBlock extends BaseElement
 {
+    use BaseElementTrait;
+
     private static $table_name = 'TrademeBlock';
     private static $singular_name = 'Trademe';
     private static $plural_name = 'Trademes';
 
     private static $inline_editable = false;
+    private static $description = 'Trademe block';
     private static $icon = 'font-icon-p-shop';
 
     public function Items($limit)
